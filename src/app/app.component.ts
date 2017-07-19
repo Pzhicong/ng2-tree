8a9766093600441ccb3b50b9a32414aee0298b63
pangzhicong <P_zhicong@163.com>
2017-07-19 10:24:21 +0800

    修改树

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,48 @@
 import { Component } from '@angular/core';
 import { LoggerService } from './logger.service';
-import { TreeModel } from 'ng2-tree';
+// import { TreeModel, NodeEvent } from 'ng2-tree';
+import * as TreeTypes from './components/ng2-tree/tree.types';
+import { NodeEvent } from './components/ng2-tree/tree.events';
 
 @Component({
   selector: 'app-root',
   // templateUrl: './app.component.html',
-  template: `<tree [tree]="tree"></tree>`,
+  template: `<tree [tree]="tree" (nodeSelected)="logEvent($event)"></tree>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
-  public tree: TreeModel = {
+  public tree: TreeTypes.TreeModel = {
     value: 'Programming languages by programming paradigm',
     children: [
       {
         value: 'Object-oriented programming',
+        id: '123',
         children: [
-          {value: 'Java'},
-          {value: 'C++'},
-          {value: 'C#'}
+          { value: 'Java' },
+          { value: 'C++' },
+          {
+            value: 'C#', children: [
+              { value: 'JavaScript' },
+              { value: 'CoffeeScript' },
+              {
+                value: 'Lua',
+                children: [
+                  { value: 'JavaScript' },
+                  { value: 'CoffeeScript' },
+                  { value: 'Lua' }
+                ]
+              }
+            ]
+          }
         ]
       },
       {
         value: 'Prototype-based programming',
         children: [
-          {value: 'JavaScript'},
-          {value: 'CoffeeScript'},
-          {value: 'Lua'}
+          { value: 'JavaScript' },
+          { value: 'CoffeeScript' },
+          { value: 'Lua' }
         ]
       }
     ]
@@ -43,5 +59,8 @@ export class AppComponent {
 
   public receive(object) {
     console.log(object);
+  }
+  public logEvent(e: NodeEvent): void {
+    console.log(e);
   }
 }
